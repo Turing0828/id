2841336300e0cddfeaf3b44378d7d603fbc69d75
h_xy <h_xy0828@163.com>
2022-05-20 22:50:59 +0800

    测试

---
+++ file/NC/src/main/java/com/niuike/Second.java
@@ -0,0 +1,15 @@
+package com.niuike;
+
+/**
+ * @Author h_xy;
+ * @Date 2022/5/19 22:02
+ * @Version 1.0
+ * @Description
+ */
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
